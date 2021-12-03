3f71420bd64eb9fcb4e0d25744ee1c742bcac272
Winder <iqwijodiro@gmail.com>
2021-12-02 22:48:20 -0400

    app to review

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,8 +3,8 @@ import colors from 'vuetify/es5/util/colors'
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    titleTemplate: '%s - Teo Academy',
-    title: 'teoacademy',
+    titleTemplate: '%s - Universidad Teológica Interamericana',
+    title: 'utionline',
     htmlAttrs: {
       lang: 'es'
     },
